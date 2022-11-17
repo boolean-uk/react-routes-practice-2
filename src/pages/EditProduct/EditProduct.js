33d87f33cf81c5a5bc6743cbd61d63b76c236d88
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-17 15:18:01 +0000

    Mapping Products and updating respective. Task Don

--- src/pages/EditProduct/EditProduct.js
+++ src/pages/EditProduct/EditProduct.js
@@ -1,38 +1,43 @@
-import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 
 function EditProductPage(props) {
-  const [productToUpdate, setProductToUpdate] = useState(null)
+  const [productToUpdate, setProductToUpdate] = useState(null);
 
-  console.log({ productToUpdate })
+  console.log({ productToUpdate });
 
-  //TODO: Write code to set the productToUpdateState
+  const location = useLocation();
 
-  //with the product data from the location.
-  const location = useLocation()
-  //
-  //Use useEffect so that when the location changes
   useEffect(() => {
-    setProductToUpdate(location.state.product)
-    console.log(productToUpdate)
-  }, [location])
-
-  //you get the product data from the location. See
-  //ViewProductPage.js to check
+    setProductToUpdate(location.state.product);
+    console.log(productToUpdate);
+  }, [location]);
 
   function handleChange(event) {
-    const name = event.target.name
-    const value = event.target.value
+    const name = event.target.name;
+    const value = event.target.value;
 
-    setProductToUpdate({ ...productToUpdate, [name]: value })
+    setProductToUpdate({ ...productToUpdate, [name]: value });
   }
 
   function handleSubmit(event) {
-    event.preventDefault()
-    props.setProducts([...props.products, productToUpdate])
+    event.preventDefault();
+    // map the products checking for the productToUpdate name
+    const updatedProducts = props.products.map((product) => {
+      if (product.id === productToUpdate.id) {
+        // Update the respective item of new Array to have the same value as product to update
+        return productToUpdate;
+      }
+      // if not the same, just return the iterated one
+      else return product;
+    });
+
+    // when found, update the name of the product
+
+    props.setProducts([...updatedProducts]);
   }
 
-  if (!productToUpdate) return <div>Loading...</div>
+  if (!productToUpdate) return <div>Loading...</div>;
 
   return (
     <form onSubmit={handleSubmit}>
@@ -46,7 +51,7 @@ function EditProductPage(props) {
       />
       <button type="submit">Edit</button>
     </form>
-  )
+  );
 }
 
-export default EditProductPage
+export default EditProductPage;
