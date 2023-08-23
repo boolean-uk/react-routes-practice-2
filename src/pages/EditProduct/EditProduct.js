c513e52801c66391980d98f38f08e7117551cb08
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-08-23 11:59:29 +0200

    solved core and extensions

--- src/pages/EditProduct/EditProduct.js
+++ src/pages/EditProduct/EditProduct.js
@@ -1,44 +1,65 @@
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function EditProductPage(props) {
+    const [productToUpdate, setProductToUpdate] = useState(null);
+    
+    //TODO: Write code to set the productToUpdateState
+    //with the product data from the location.
+    //
+    //Use useEffect so that when the location changes
+    //you get the product data from the location. See
+    //ViewProductPage.js to check
 
-  const [productToUpdate, setProductToUpdate] = useState(null);
+    const { products, setProducts } = props;
 
-  console.log({ productToUpdate });
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  //TODO: Write code to set the productToUpdateState
-  //with the product data from the location. 
-  // 
-  //Use useEffect so that when the location changes
-  //you get the product data from the location. See
-  //ViewProductPage.js to check
+    useEffect(() => {
+        if (location.state) {
+            setProductToUpdate(location.state.product);
+        }
+    }, []);
 
-  function handleChange(event) {
-    const name = event.target.name;
-    const value = event.target.value;
+    function handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
 
-    setProductToUpdate({ ...productToUpdate, [name]: value });
-  }
+        setProductToUpdate({ ...productToUpdate, [name]: value });
+    }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
+        const updatedProducts = products.map((p) =>
+            p.id === productToUpdate.id ? productToUpdate : p
+        );
+        setProducts(updatedProducts);
+        navigate("/products");
+    }
 
-  if (!productToUpdate) return <div>Loading...</div>;
+    if (!productToUpdate) return <div>Loading...</div>;
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Product Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        onChange={handleChange}
-        value={productToUpdate.name}
-      />
-      <button type="submit">Edit</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Product Name</label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                onChange={handleChange}
+                value={productToUpdate.name}
+            />
+              <input
+                type="text"
+                id="price"
+                name="price"
+                onChange={handleChange}
+                value={productToUpdate.price}
+            />
+            <button type="submit">Edit</button>
+        </form>
+    );
 }
 
 export default EditProductPage;
--- src/pages/Products/ProductsPage.js
+++ src/pages/Products/ProductsPage.js
@@ -1,33 +1,45 @@
 import { Link } from "react-router-dom";
 
 function ProductsPage(props) {
-  console.log("Inside ProductsPage: ", { props });
+    console.log("Inside ProductsPage: ", { props });
 
-  const { products } = props;
+    const { products } = props;
 
-  return (
-    <main>
-      <h2>Products</h2>
-      <ul>
-        {products.map((product, index) => {
-          return (
-            <li key={index}>
-              <h3>{product.name}</h3>
-              <p>£{product.price}</p>
-              <Link to={`/products/${product.id}`} state={{ product }}>
-                View Product
-              </Link>
-              {/* 
+    return (
+        <main>
+            <h2>Products</h2>
+            <ul>
+                {products.map((product, index) => {
+                    return (
+                        <li key={index}>
+                            <h3>{product.name}</h3>
+                            <p>£{product.price}</p>
+                            <Link
+                                to={`/products/${product.id}`}
+                                state={{ product }}
+                            >
+                                View Product
+                            </Link>
+                            <li>
+                                <Link
+                                    to={`/products/${product.id}/edit`}
+                                    state={{ product }}
+                                >
+                                    EDIT
+                                </Link>
+                            </li>
+
+                            {/* 
                 TODO: Add a link to the edit page here 
                 Pass the product data as state in the link, just
                 like we are doing for the view product link above
               */}
-            </li>
-          );
-        })}
-      </ul>
-    </main>
-  );
+                        </li>
+                    );
+                })}
+            </ul>
+        </main>
+    );
 }
 
 export default ProductsPage;
