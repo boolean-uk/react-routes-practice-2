30f16538a13d5cb3f0c3580ba1b004cf3f236a10
Nazar Tymiv <nazartumiv@gmail.com>
2023-11-03 12:20:25 +0000

    Finished exercise and extension as well :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
-import { useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import { appsData, productsData } from './data.js';
-import Home from './pages/Home/Home';
-import ProductsPage from './pages/Products/ProductsPage';
-import ViewProductPage from './pages/ViewProduct/ViewProductPage';
-import './App.css'
+import { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { appsData, productsData } from "./data.js";
+import Home from "./pages/Home/Home";
+import ProductsPage from "./pages/Products/ProductsPage";
+import ViewProductPage from "./pages/ViewProduct/ViewProductPage";
+import "./App.css";
+import EditProductPage from "./pages/EditProduct/EditProduct.jsx";
 
 export default function App() {
-  const [products, setProducts] = useState(productsData);
-  const [apps, setApps] = useState(appsData);
+    const [products, setProducts] = useState(productsData);
+    const [apps, setApps] = useState(appsData);
 
-  console.log({ products, apps });
+    // console.log({ products, apps });
 
-  return (
-    <div className="App">
-      <header>
-        <h1>Apple Shop</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/products">Products</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Routes>
-        {/*
-         TODO: add your route for editing a product here. The
-         Route should be /products/:id/edit and it should use
-         the EditProduct element
-         */}
-        <Route path="/products/:id" element={<ViewProductPage />} />
-        <Route path="/products" element={<ProductsPage products={products} />}/>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
-  );
+    const handleEditProducts = (product) => {
+        setProducts(
+            products.map((item) =>
+                item.id === product.id ? { ...item, name: product.name } : item
+            )
+        );
+    };
+
+    return (
+        <div className="App">
+            <header>
+                <h1>Apple Shop</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <Routes>
+                <Route
+                    path="/products/:id/edit"
+                    element={
+                        <EditProductPage
+                            handleEditProducts={handleEditProducts}
+                        />
+                    }
+                />
+                <Route path="/products/:id" element={<ViewProductPage />} />
+                <Route
+                    path="/products"
+                    element={<ProductsPage products={products} />}
+                />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
--- src/pages/Products/ProductsPage.jsx
+++ src/pages/Products/ProductsPage.jsx
@@ -1,33 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function ProductsPage(props) {
-  console.log("Inside ProductsPage: ", { props });
+    console.log("Inside ProductsPage: ", { props });
 
-  const { products } = props;
+    const { products } = props;
 
-  return (
-    <main>
-      <h2>Products</h2>
-      <ul>
-        {products.map((product, index) => {
-          return (
-            <li key={index}>
-              <h3>{product.name}</h3>
-              <p>£{product.price}</p>
-              <Link to={`/products/${product.id}`} state={{ product }}>
-                View Product
-              </Link>
-              {/*
-                TODO: Add a link to the edit page here
-                Pass the product data as state in the link, just
-                like we are doing for the view product link above
-              */}
-            </li>
-          );
-        })}
-      </ul>
-    </main>
-  );
+    return (
+        <main>
+            <h2>Products</h2>
+            <ul>
+                {products.map((product, index) => {
+                    return (
+                        <li key={index}>
+                            <h3>{product.name}</h3>
+                            <p>£{product.price}</p>
+                            <Link
+                                to={`/products/${product.id}`}
+                                state={{ product }}
+                            >
+                                View Product
+                            </Link>
+                            <br />
+                            <Link
+                                to={`/products/${product.id}/edit`}
+                                state={{ product }}
+                            >
+                                Edit
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </main>
+    );
 }
 
 export default ProductsPage;
